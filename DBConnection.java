4a5537cb8a02ed15101323f90ea880d4dbc8aee8
keshar18 <keshargupta6695@gmail.com>
2026-03-21 14:18:07 +0530

    add database

--- DBConnection.java
+++ DBConnection.java
@@ -1,3 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
-    
+
+    public static Connection getConnection() {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/nivarti",
+                "root",
+                "root"    
+            );
+            return con;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
